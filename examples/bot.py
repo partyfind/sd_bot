01259609da29138b9307b098aa8611b46c781da1
Mikhail Guseletov <mvguseletov@bars.group>
2023-02-17 11:52:47 +0700

    улучшалка, не работает отправка нескольких фото, не работает

--- examples/bot.py
+++ examples/bot.py
@@ -31,8 +31,8 @@ def create_post(type: str):
     for row in rows:
         if type == 'min':
             steps = 20
-            width = 512
-            height = 512
+            width = 256
+            height = 256
             cfg_scale = 7
         elif type == 'max':
             steps = 100
@@ -59,8 +59,8 @@ def create_post(type: str):
             prompt = 'ghibli style '+prompt
         elif row[5].find('future') != -1:
             prompt = 'future style '+prompt
-        elif row[5].find('anime-pencil') != -1:
-            prompt = 'anime pencil concept style '+prompt
+        elif row[5].find('vectorArt') != -1:
+            prompt = 'vector style '+prompt
         #prompt = '```'+prompt+'```'
         data = {
             'prompt': prompt,
@@ -70,8 +70,10 @@ def create_post(type: str):
             'cfg_scale': cfg_scale,
             'model':row[5],
             'negative_prompt': row[6],
-            'sampler_index': row[7]
+            'sampler_index': row[7],
+            'batch_size': 2
         }
+        # для вывода в телегу
         data2 = {
             #'prompt': prompt,
             'steps':  steps,
@@ -86,6 +88,7 @@ def create_post(type: str):
     #print(len(data))
     response = submit_post(txt2img_url, data)
     save_encoded_image(response.json()['images'][0], 'dog.png')
+    save_encoded_image(response.json()['images'][1], 'dog2.png')
     print('capture save')
     return data2
 
@@ -140,10 +143,17 @@ async def send_welcome(message: types.Message):
 @dp.callback_query_handler(text='min')
 async def cb_menu_1(callback: types.CallbackQuery) -> None:
     data = create_post('min')
-    with open('dog.png', 'rb') as photo:
-        await callback.message.delete()
-        await callback.message.answer_photo(photo, caption=data, reply_markup=types.ReplyKeyboardRemove())
-        await bot.send_message(chat_id=callback.from_user.id, text='Выбираем заново', reply_markup=get_ikb())
+    #with open('dog.png', 'rb') as photo:
+    #    photo1 = photo
+    #with open('dog2.png', 'rb') as photo:
+    #    photo2 = photo
+    #media = types.MediaGroup()
+    #media.attach_photo(types.InputFile('dog.png'), 'Превосходная фотография')
+    #media.attach_photo(types.InputFile('dog2.png'), 'Превосходная фотография 2')
+    #await bot.reply_media_group(media=media)
+    #await callback.message.delete()
+    #await callback.message.answer_photo(photo, caption=data, reply_markup=types.ReplyKeyboardRemove())
+    #await bot.send_message(chat_id=callback.from_user.id, text='Выбираем заново', reply_markup=get_ikb())
 
 
 
@@ -178,15 +188,24 @@ def get_size() -> InlineKeyboardMarkup:
          ],[
          InlineKeyboardButton('512*512',  callback_data='size|512_512'),
          InlineKeyboardButton('512*768',  callback_data='size|512_768'),
-         InlineKeyboardButton('512*1024', callback_data='size|512_1024')
+         InlineKeyboardButton('512*1024', callback_data='size|512_1024'),
+         InlineKeyboardButton('512*1280', callback_data='size|512_1280')
          ],[
          InlineKeyboardButton('768*512',  callback_data='size|768_512'),
          InlineKeyboardButton('768*768',  callback_data='size|768_768'),
-         InlineKeyboardButton('768*1024', callback_data='size|768_1024')
+         InlineKeyboardButton('768*1024', callback_data='size|768_1024'),
+         InlineKeyboardButton('768*1280', callback_data='size|768_1280')
          ],[
          InlineKeyboardButton('1024*512', callback_data='size|1024_512'),
          InlineKeyboardButton('1024*768', callback_data='size|1024_768'),
-         InlineKeyboardButton('1024*1024', callback_data='size|1024_1024')],[
+         InlineKeyboardButton('1024*1024', callback_data='size|1024_1024'),
+         InlineKeyboardButton('1024*1280', callback_data='size|1024_1280')
+         ],[
+         InlineKeyboardButton('1280*512', callback_data='size|1280_512'),
+         InlineKeyboardButton('1280*768', callback_data='size|1280_768'),
+         InlineKeyboardButton('1280*1024', callback_data='size|1280_1024'),
+         InlineKeyboardButton('1280*1280', callback_data='size|1280_1280')
+         ],[
          InlineKeyboardButton('gen', callback_data='gen')
          ]
     ])
@@ -200,6 +219,9 @@ def get_scale() -> InlineKeyboardMarkup:
          InlineKeyboardButton('8',  callback_data='scale|8'),
          InlineKeyboardButton('9',  callback_data='scale|9'),
          InlineKeyboardButton('10', callback_data='scale|10')],[
+         InlineKeyboardButton('11', callback_data='scale|11'),
+         InlineKeyboardButton('12', callback_data='scale|12'),
+         InlineKeyboardButton('13', callback_data='scale|13'),
          InlineKeyboardButton('15', callback_data='scale|15'),
          InlineKeyboardButton('20', callback_data='scale|20')],[
          InlineKeyboardButton('gen', callback_data='gen')]
@@ -211,7 +233,7 @@ def get_steps() -> InlineKeyboardMarkup:
         [InlineKeyboardButton('20',  callback_data='steps|20'),
          InlineKeyboardButton('30',  callback_data='steps|30'),
          InlineKeyboardButton('40',  callback_data='steps|40'),
-         InlineKeyboardButton('50',  callback_data='steps|50')],[
+         InlineKeyboardButton('50',  callback_data='steps|50'),
          InlineKeyboardButton('60',  callback_data='steps|60'),
          InlineKeyboardButton('80',  callback_data='steps|80'),
          InlineKeyboardButton('100', callback_data='steps|100')],[
