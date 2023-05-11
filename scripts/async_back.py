b4541fd5a5ab7f2313a5478bf191b57826baceb2
Mikhail Guseletov <mvguseletov@bars.group>
2023-05-11 14:43:49 +0700

    удачно, но не до конца

--- scripts/async_back.py
+++ scripts/async_back.py
@@ -6,6 +6,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import subprocess
 import json
 import requests
+import time
 
 API_TOKEN = '900510503:AAG5Xug_JEERhKlf7dpOpzxXcJIzlTbWX1M'
 
@@ -20,6 +21,7 @@ def getOpt() -> InlineKeyboardMarkup:
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message) -> None:
+    print('start')
     await message.reply('Тестирование асинхрона.\ntime - запускает бесконечный счётчик \n/info - выводит сообщение в телегу', reply_markup=getOpt())
 
 @dp.callback_query_handler(text='time')
@@ -32,10 +34,14 @@ async def time(callback: types.CallbackQuery) -> None:
 async def start_info(message: types.Message):
     print('info')
     response = requests.get('http://127.0.0.1:7861/sdapi/v1/progress?skip_current_image=false', data=json.dumps(''))
-    print(response.json())
-    print(response.json()['eta_relative'])
-    await bot.send_message(chat_id=message.from_user.id, text=response.json()['eta_relative'])
-    await message.answer("Проверка асинхрона - info")
+    e = response.json()['eta_relative']
+    while e > 0 and e != '0.0':
+        #time.sleep(2)
+        response = requests.get('http://127.0.0.1:7861/sdapi/v1/progress?skip_current_image=false', data=json.dumps(''))
+        e2 = response.json()['eta_relative']
+        await bot.send_message(chat_id=message.from_user.id, text=e2)
+    await bot.send_message(chat_id=message.from_user.id, text='Конец')
+    #await message.answer("Проверка асинхрона - info")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
