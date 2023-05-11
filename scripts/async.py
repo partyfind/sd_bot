c3280d28f461ed05f4d5ee0cc90270e9191157d7
Mikhail Guseletov <mvguseletov@bars.group>
2023-05-11 11:43:52 +0700

    скрипт асинхронногых команд обработка инлайновых кнопок в одной функции

--- scripts/async.py
+++ scripts/async.py
@@ -1,56 +1,33 @@
 # 900510503:AAG5Xug_JEERhKlf7dpOpzxXcJIzlTbWX1M
 # https://docs.aiogram.dev/en/latest/
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 import asyncio
-import subprocess
-from aiogram import Bot, Dispatcher, executor, types
+from datetime import datetime
 
-bot = Bot(token="900510503:AAG5Xug_JEERhKlf7dpOpzxXcJIzlTbWX1M")
+API_TOKEN = '900510503:AAG5Xug_JEERhKlf7dpOpzxXcJIzlTbWX1M'
+
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message) -> None:
+    await message.reply('Тестирование асинхрона.\n/time - запускает бесконечный счётчик \n/info - выводит сообщение в телегу')
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
-    launch_button = types.InlineKeyboardButton(text="Launch", callback_data="launch")
-    stop_button = types.InlineKeyboardButton(text="Stop", callback_data="stop")
-    keyboard.row(launch_button, stop_button)
-    await bot.send_message(chat_id=message.chat.id, text="Choose an action:", reply_markup=keyboard)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'launch')
-async def process_launch_callback(callback_query: types.CallbackQuery):
-    process = subprocess.Popen(["python", "time.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    await bot.answer_callback_query(callback_query.id)
-    msg = await bot.send_message(chat_id=callback_query.message.chat.id, text="Launching...")
-    print(25)
+async def get_time():
     while True:
-        print(27)
-        # Получаем новые сообщения из консоли
-        output = process.stdout.readline().decode().strip()
-        # Если нет новых сообщений, ждем 1 секунду и продолжаем проверку
-        if not output:
-            print(30)
-            await asyncio.sleep(1)
-            continue
-        # Отправляем новое сообщение в чат и редактируем предыдущее
-        print(output)
-        prev_msg = await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=msg.message_id,
-                                               text=output)
-        # Удаляем предыдущее сообщение и продолжаем цикл
-        await prev_msg.delete()
+        await asyncio.sleep(2)
+        time = datetime.now().strftime("%H:%M:%S")
+        print(f"Этот цикл продолжится бесконечно! {time}")
 
+@dp.message_handler(commands=['time'])
+async def start_time(message: types.Message):
+    await message.answer("Time started")
+    asyncio.create_task(get_time())
 
-@dp.callback_query_handler(lambda c: c.data == 'stop')
-async def process_stop_callback(callback_query: types.CallbackQuery):
-    print(45)
-    subprocess.run(["pkill", "-f", "time.py"])
-    keyboard = types.InlineKeyboardMarkup()
-    launch_button = types.InlineKeyboardButton(text="Launch", callback_data="launch")
-    stop_button = types.InlineKeyboardButton(text="Stop", callback_data="stop")
-    keyboard.row(launch_button, stop_button)
-    await bot.edit_message_text(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id,
-                                text="Stopped", reply_markup=keyboard)
-
+@dp.message_handler(commands=['info'])
+async def start_info(message: types.Message):
+    await message.answer("Проверка асинхрона - info")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
