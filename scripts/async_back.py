b40ea511ce0cc709b7151b55323105252c79c2a8
Mikhail Guseletov <mvguseletov@bars.group>
2023-05-11 13:08:57 +0700

    проба поймать асинхрон с файла и вывести в телегу не получается

--- scripts/async_back.py
+++ scripts/async_back.py
@@ -2,7 +2,7 @@
 # https://docs.aiogram.dev/en/latest/
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import asyncio
 import subprocess
 
 API_TOKEN = '900510503:AAG5Xug_JEERhKlf7dpOpzxXcJIzlTbWX1M'
@@ -10,30 +10,56 @@ API_TOKEN = '900510503:AAG5Xug_JEERhKlf7dpOpzxXcJIzlTbWX1M'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-def getOpt() -> InlineKeyboardMarkup:
-    opt = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton('time', callback_data='time')]
-    ])
-    return opt
-
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message) -> None:
-    await message.reply('Тестирование асинхрона.\ntime - запускает бесконечный счётчик \n/info - выводит сообщение в телегу', reply_markup=getOpt())
-
-@dp.callback_query_handler(text='time')
-async def time(callback: types.CallbackQuery) -> None:
-    print('time')
-    await callback.message.edit_text('time start callback')
-    process = subprocess.Popen(['python', 'time.py', '--nowebui', '--xformers'], stderr=subprocess.PIPE)
+async def read_pipe(pipe, label, user):
+    print('read_pipe')
+    # Читаем строки из стандартного вывода скрипта
     while True:
-        txt = process.stderr.readline().decode().strip()
-        if txt == '':
-            txt = 'Ждёмс'
-        await callback.message.edit_text(txt)
+        # Читаем строку из стандартного вывода скрипта
+        line = await pipe.readline()
+        print(19)
+        print(line)
+
+        # Если строка пустая, значит процесс завершился
+        if line == b'':
+            break
+
+        # Декодируем строку в utf-8 и отправляем ее в чат
+        #output = line.decode('utf-8').strip()
+        await bot.send_message(user, f'{label}: {line}')
+
+async def get_time(user):
+    while True:
+        await asyncio.sleep(2)
+        #time = subprocess.check_output(['python', 'time.py']).decode('utf-8').strip()
+        #process = subprocess.check_output(['python', 'time.py'], stdout=subprocess.PIPE)
+        #print(process.stdout)
+        #print(f"Этот цикл продолжится бесконечно! {process}")
+        process = await asyncio.create_subprocess_exec(
+            'python', 'time.py',
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        print(41)
+        print(process.stdout.decode('utf-8'))
+        #print(subprocess.PIPE)
+        #asyncio.create_task(read_pipe(process.stdout, 'stdout', user))
+
+
+
+async def get_info(message: types.Message):
+    await message.answer("Это бот")
+
+
+@dp.message_handler(commands=['time'])
+async def start_time(message: types.Message):
+    await message.answer("Time started")
+    asyncio.create_task(get_time(message.from_user.id))
+
 
 @dp.message_handler(commands=['info'])
 async def start_info(message: types.Message):
-    await message.answer("Проверка асинхрона - info")
+    await message.answer("Info started")
+    await get_info(message)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- scripts/time.py
+++ scripts/time.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 import time
 while True:
-    print("Этот цикл продолжится бесконечно!"+datetime.now().strftime("%H:%M:%S"))
+    print(" ttt Этот цикл продолжится бесконечно!"+datetime.now().strftime("%H:%M:%S"))
     time.sleep(2)
