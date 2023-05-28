e7e11295f7569155c33323473f511a73f3d8f618
Mikhail Guseletov <mvguseletov@bars.group>
2023-05-29 02:25:11 +0700

    парсинг с сохранением. Не работает

--- scripts/json_parse3.py
+++ scripts/json_parse3.py
@@ -27,10 +27,19 @@ def get_json_keys():
     for key, val in data.items():
         arr.append(key)
     return arr
-get_json_keys()
 
-class Registration(StatesGroup):
+# https://aiogram-birdi7.readthedocs.io/en/latest/examples/finite_state_machine_example.html
+"""class Form(StatesGroup):
     text = State()
+    num = State()
+for key in data:
+    print(key)
+    setattr(Form, key, State())"""
+class Form(StatesGroup):
+    def __init__(self, data):
+        for key in data:
+            setattr(self, key, State())
+        self.data = data
 
 # Команда /get_json для вывода списка параметров
 @dp.message_handler(commands=['get_json'])
@@ -42,23 +51,53 @@ async def get_json(message: types.Message):
 # Ввели любой текст
 @dp.message_handler(lambda message: True)
 async def change_json(message: types.Message):
+    print(5444)
+    print(Form)
     str = message.text
     if str == '/text':
         await message.answer('Привет! Напиши любой текст:')
-        await Registration.text.set()
-    for key in data:
+        await Form.text.set()
+    elif str == '/num':
+        await message.answer('Привет! Напиши любое число:')
+        await Form.num.set()
+    elif str == '/enable_hr':
+        await message.answer('Привет! Напиши любое enable_hr:')
+        await Form.enable_hr.set()
+"""    for key in data:
         # Если ключ json равен вводимой команде И остальное пусто (TODO оптимизировать проверку на пустоту)
         if key == str.split()[0][1:] and str.split()[1:] == []:
             await message.answer('Вы ввели пустую команду' + str + ', введите новое значение ниже либо напишите '+str+' + новое_значение')
-        elif key in str:
-            found_key = key
-            #TODO починить одинаковость subseed_strength seed
-            substring = str[str.index(key) + len(key):]
-            data[found_key.split()[0][0:]] = substring
+        elif key in str: # TODO оптимизировать поиск (починить одинаковость subseed_strength seed)
+            substring = str[str.index(key) + len(key):] # вычисляем, до какой строки режем
+            data[key.split()[0][0:]] = substring.split()[0][0:] # /prompt = < >текст. Удаляем / и пробел < > через split
             print(data)
-            await message.answer('Вы ввели '+str+', теперь '+found_key+' = '+substring)
+            await message.answer('Вы ввели '+str+', теперь '+key+' = '+substring)"""
 
-@dp.message_handler(state=Registration.text)
+@dp.message_handler(state=Form)
+async def gender_handler(message: types.Message, state: FSMContext):
+    print(63)
+    async with state.proxy() as data2:
+        current_state = await state.get_state() # Form:команда
+        print(65)
+        print(data2)
+        print(68)
+        print(state)
+        print(current_state)
+        for key in data:
+            if current_state == 'Form:'+key:
+                print(data2[key])
+                data2[key] = message.text
+        #if current_state == 'Form:num':
+        #    print('nummm')
+        #if current_state == 'Form:text':
+        #    print('texttt')
+        await state.reset_state()
+        #data2['text'] = message.text
+        await message.answer("Спасибос за "+message.text)
+        # После завершения беседы сбрасываем состояние пользователя:
+        #await state.reset_state()
+
+"""@dp.message_handler(state=Form.text)
 async def gender_handler(message: types.Message, state: FSMContext):
     async with state.proxy() as data2:
         print(data2)
@@ -67,6 +106,16 @@ async def gender_handler(message: types.Message, state: FSMContext):
                               f" text {data2['text']}, ")
         # После завершения беседы сбрасываем состояние пользователя:
         await state.reset_state()
+
+@dp.message_handler(state=Form.num)
+async def gender_handler(message: types.Message, state: FSMContext):
+    async with state.proxy() as data2:
+        print(data2)
+        data2['num'] = message.text
+        await message.answer("Спасибо за число!\n"
+                              f" num {data2['num']}, ")
+        # После завершения беседы сбрасываем состояние пользователя:
+        await state.reset_state()"""
 
 # Запуск бота
 if __name__ == '__main__':
