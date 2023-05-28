dde8c5aafb8892e45e1826687405141b783c98d2
Mikhail Guseletov <mvguseletov@bars.group>
2023-05-29 01:09:26 +0700

    /text сохраняет текст после ответа

--- scripts/json_parse3.py
+++ scripts/json_parse3.py
@@ -1,19 +1,15 @@
 import json
 from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher import filters
 from aiogram.utils import executor
-import json
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.utils import executor
-from aiogram.dispatcher.filters import Command
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 
 # Инициализация бота и диспетчера
 API_TOKEN = '900510503:AAG5Xug_JEERhKlf7dpOpzxXcJIzlTbWX1M'
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 # JSON данные
 # TODO http://127.0.0.1:7861/openapi.json #/components/schemas/StableDiffusionProcessingTxt2Img
@@ -33,6 +29,9 @@ def get_json_keys():
     return arr
 get_json_keys()
 
+class Registration(StatesGroup):
+    text = State()
+
 # Команда /get_json для вывода списка параметров
 @dp.message_handler(commands=['get_json'])
 async def get_json(message: types.Message):
@@ -44,6 +43,9 @@ async def get_json(message: types.Message):
 @dp.message_handler(lambda message: True)
 async def change_json(message: types.Message):
     str = message.text
+    if str == '/text':
+        await message.answer('Привет! Напиши любой текст:')
+        await Registration.text.set()
     for key in data:
         # Если ключ json равен вводимой команде И остальное пусто (TODO оптимизировать проверку на пустоту)
         if key == str.split()[0][1:] and str.split()[1:] == []:
@@ -55,6 +57,16 @@ async def change_json(message: types.Message):
             data[found_key.split()[0][0:]] = substring
             print(data)
             await message.answer('Вы ввели '+str+', теперь '+found_key+' = '+substring)
+
+@dp.message_handler(state=Registration.text)
+async def gender_handler(message: types.Message, state: FSMContext):
+    async with state.proxy() as data2:
+        print(data2)
+        data2['text'] = message.text
+        await message.answer("Спасибо за текст!\n"
+                              f" text {data2['text']}, ")
+        # После завершения беседы сбрасываем состояние пользователя:
+        await state.reset_state()
 
 # Запуск бота
 if __name__ == '__main__':
