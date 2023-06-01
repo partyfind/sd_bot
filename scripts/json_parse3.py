c7bc03b5e80a124aba7297ca663023988645a144
Mikhail Guseletov <mvguseletov@bars.group>
2023-06-01 15:16:50 +0700

    рефакторинг

--- scripts/json_parse3.py
+++ scripts/json_parse3.py
@@ -1,4 +1,3 @@
-import json
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -22,13 +21,6 @@ data = {
 }
 
 # https://aiogram-birdi7.readthedocs.io/en/latest/examples/finite_state_machine_example.html
-class Form2(StatesGroup):
-    enable_hr = State()
-    prompt = State()
-    seed = State()
-    override_settings_restore_afterwards = State()
-    subseed_strength = State()
-
 # Dynamically create a new class with the desired attributes
 state_classes = {}
 for key in data:
@@ -37,9 +29,6 @@ for key in data:
 # Inherit from the dynamically created class
 Form = type('Form', (StatesGroup,), state_classes)
 
-#Form = type("Form", (StatesGroup,), data)
-#form = Form(data)
-#print(form)
 # Команда /get_json для вывода списка параметров
 @dp.message_handler(commands=['get_json'])
 async def get_json(message: types.Message):
@@ -50,91 +39,29 @@ async def get_json(message: types.Message):
 # Ввели любой текст
 @dp.message_handler(lambda message: True)
 async def change_json(message: types.Message):
-    print(5444)
-    print(Form)
     str2 = message.text
-    print(45)
-    print(str2.split()[0]) #/enable_hr
-    print(str2.split()[0][1:]) #enable_hr
-    print(str2.split()[1:])
-    print(49)
     nam = str2.split()[0][1:]
-    if str2 == '/prompt':
-        await message.answer('Привет! Напиши любой prompt:')
-        await Form.prompt.set()
-    elif str2 == '/num':
-        await message.answer('Привет! Напиши любое число:')
-        await Form.num.set()
-    elif str2 == '/enable_hr':
-        await message.answer('Привет! Напиши любое enable_hr:')
-        print(58)
-        attrs = dir(Form)
-        print(attrs)
-        print(attrs[0])
-        state_names = [attr for attr in attrs if isinstance(getattr(Form, attr), State)]
-        print(state_names)
+    attrs = dir(Form)
+    state_names = [attr for attr in attrs if isinstance(getattr(Form, attr), State)]
+    if nam in state_names:
+        await message.answer('Напиши любое '+nam)
         if nam in state_names:
             await getattr(Form, nam).set()
         else:
             print('ОШибка какая-то')
-"""    for key in data:
-        # Если ключ json равен вводимой команде И остальное пусто (TODO оптимизировать проверку на пустоту)
-        if key == str.split()[0][1:] and str.split()[1:] == []:
-            await message.answer('Вы ввели пустую команду' + str + ', введите новое значение ниже либо напишите '+str+' + новое_значение')
-        elif key in str: # TODO оптимизировать поиск (починить одинаковость subseed_strength seed)
-            substring = str[str.index(key) + len(key):] # вычисляем, до какой строки режем
-            data[key.split()[0][0:]] = substring.split()[0][0:] # /prompt = < >текст. Удаляем / и пробел < > через split
-            print(data)
-            await message.answer('Вы ввели '+str+', теперь '+key+' = '+substring)"""
 
+# Ввели ответ на change_json
 @dp.message_handler(state=Form)
-async def gender_handler(message: types.Message, state: FSMContext):
-    print(63)
+async def answer_handler(message: types.Message, state: FSMContext):
     async with state.proxy() as data2:
         current_state = await state.get_state() # Form:команда
-        print(65)
-        print(data2)
-        print(68)
-        print(state)
-        print(current_state)
         for key, val in data.items():
-            print(82)
-            print(key)
             if current_state == 'Form:'+key:
                 print(85)
-                #print(data[val])
                 data[key] = message.text
-        #if current_state == 'Form:num':
-        #    print('nummm')
-        #if current_state == 'Form:text':
-        print(91)
-        print(data2)
-        print(data2)
+                break
         await state.reset_state()
-        #data2['text'] = message.text
-        await message.answer("Спасибос за "+message.text)
-        # После завершения беседы сбрасываем состояние пользователя:
-        #await state.reset_state()
-
-"""@dp.message_handler(state=Form.text)
-async def gender_handler(message: types.Message, state: FSMContext):
-    async with state.proxy() as data2:
-        print(data2)
-        data2['text'] = message.text
-        await message.answer("Спасибо за текст!\n"
-                              f" text {data2['text']}, ")
-        # После завершения беседы сбрасываем состояние пользователя:
-        await state.reset_state()
-
-@dp.message_handler(state=Form.num)
-async def gender_handler(message: types.Message, state: FSMContext):
-    async with state.proxy() as data2:
-        print(data2)
-        data2['num'] = message.text
-        await message.answer("Спасибо за число!\n"
-                              f" num {data2['num']}, ")
-        # После завершения беседы сбрасываем состояние пользователя:
-        await state.reset_state()"""
+        await message.answer(key+' = '+message.text+'\n\n/get_json')
 
 # Запуск бота
 if __name__ == '__main__':
